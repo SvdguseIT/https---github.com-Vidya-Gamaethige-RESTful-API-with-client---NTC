ce5896523da3c2cc6c2404f766ccd1a65f12ed89
svdguseIt <svdguseit@gmail.com>
2025-04-28 09:36:41 +0530

    adminMiddleware.js update

--- middleware/adminMiddleware.js
+++ middleware/adminMiddleware.js
@@ -1,3 +1,4 @@
+//adminMiddleware.js
 const adminMiddleware = (req, res, next) => {
     if (req.user.role !== 'admin') {
       return res.status(403).json({ error: 'Access denied. Admin privileges required.' });
